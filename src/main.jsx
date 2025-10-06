a62fbead921b7e07eed5f5c11ea59118af562f2c
Harsha Vardhan <harshavardhan3011@example.com>
2025-10-06 18:11:04 +0530

    Fix folder name case mismatch with import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { ThemeProvider } from "./contexts/ThemeContext";
+import { ThemeProvider } from "./Contexts/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
